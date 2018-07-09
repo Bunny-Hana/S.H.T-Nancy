546dc1ea551e26915276c7455e5abdc615bf36e1
Bunny-Hana <40865087+Bunny-Hana@users.noreply.github.com>
2018-07-09 10:00:10 +0900

    Update Testancy.py

--- Testancy.py
+++ Testancy.py
@@ -53,7 +53,7 @@ async def on_message(message):
     elif 'S.H.T' in message.content:
         await client.send_message(message.channel, 'Did anyone called me?')
         
-    elif message.content.startswith('실험 戊호!'):
+    elif message.content.startswith('Experiment Num.甲!'):
         async for log in client.logs_from(message.channel, limit=1):
             if log.author == message.author:
                 await client.delete_message(log)
